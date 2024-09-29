526f19af4290d44594c3f38c2a6fae071488c7eb
Nazmos27 <nazmos789@gmail.com>
2024-09-29 18:21:34 +0600

    dynamic section header,service and featured section done

---
+++ src/components/shared/SectionHeader.tsx
@@ -0,0 +1,17 @@
+const SectionHeader = ({
+    title,
+    description,
+  }: {
+    title: string;
+    description: string;
+  }) => {
+    return (
+      <div className="text-center">
+        <h2 className="text-3xl text-blue-500 font-semibold">{title}</h2>
+        <p className="lg:w-2/3  mx-auto">{description}</p>
+      </div>
+    );
+  };
+  
+  export default SectionHeader;
+  
---
+++ src/pages/Home/FeaturedSection.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { TProduct, useGetAllProductsQuery } from "../../redux/api/productsApi";
+import Loading from "../../components/Loading";
+import ProductCard from "../../components/ProductCard";
+import SectionHeader from "../../components/shared/SectionHeader";
+
+const FeaturedProducts: React.FC = () => {
+  const { data, isLoading } = useGetAllProductsQuery({ limit: 6, page: 1 });
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="my-12">
+      <SectionHeader
+        title="Featured Keyboard"
+        description="Discover our curated selection of the latest and top-rated mechanical keyboards. Each model blends performance, style, and value. Find your perfect keyboard today"
+      />
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 my-6">
+        {data?.data?.map((product: TProduct) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
